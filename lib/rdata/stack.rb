1bcad67bb2b1d9c4d86a15b347f00fee0cf29ebb
Matteo Centenaro <bugant@gmail.com>
2013-12-04 22:31:03 +0100

    FIX: Ruby use 2 spaces indent

--- lib/rdata/stack.rb
+++ lib/rdata/stack.rb
@@ -9,27 +9,27 @@ This is the Stack class. It allows easy implementation of stacks and use of thei
 =end
 
 module RData
-    class Stack
-      def initialize
-        @stack = []
-      end
-
-      def top
-        @stack.last
-      end
-
-      def push(x)
-        @stack << x
-        x
-      end
-
-      def pop
-        raise '[underflow] Cannot pop data from an empty stack' if empty?
-        @stack.pop
-      end
-
-      def empty?
-        @stack.empty?
-      end
+  class Stack
+    def initialize
+      @stack = []
     end
+
+    def top
+      @stack.last
+    end
+
+    def push(x)
+      @stack << x
+      x
+    end
+
+    def pop
+      raise '[underflow] Cannot pop data from an empty stack' if empty?
+      @stack.pop
+    end
+
+    def empty?
+      @stack.empty?
+    end
+  end
 end
